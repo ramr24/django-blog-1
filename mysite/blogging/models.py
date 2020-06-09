69397e61ac23881d1fa980f87377bc5b26ebf8c5
Ramkumar Rajanbabu <kumar.rajanbabu@gmail.com>
2020-06-08 21:04:12 -0700

    at start of django admin and url resolution

--- mysite/blogging/models.py
+++ mysite/blogging/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+# blogging/models.py
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+	title = models.CharField(max_length=128)
+	text = models.TextField(blank=True)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	created_date = models.DateTimeField(auto_now_add=True)
+	modified_date = models.DateTimeField(auto_now_add=True)
+	published_date = models.DateTimeField(blank=True, null=True)
