8cb821d0e7998c9d918ee6d6da70c5bfdfbfb79b
Ramkumar Rajanbabu <kumar.rajanbabu@gmail.com>
2020-06-15 13:38:59 -0700

    finished project url space

--- mysite/blogging/tests.py
+++ mysite/blogging/tests.py
@@ -3,6 +3,8 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from blogging.models import Post, Category
+import datetime
+from django.utils.timezone import utc
 
 class PostTestCase(TestCase):
 	fixtures = ['blogging_test_fixture.json',]
@@ -23,3 +25,22 @@ class CategoryTestCase(TestCase):
 		c1 = Category(name=expected)
 		actual = str(c1)
 		self.assertEqual(expected, actual)
+
+
+class FrontEndTestCase(TestCase):
+	"""test views provided in the front-end"""
+	fixtures = ['blogging_test_fixture.json']
+
+	def setUp(self):
+		self.now = datetime.datetime.utcnow().replace(tzinfo=utc)
+		self.timedelta = datetime.timedelta(15)
+		author = User.objects.get(pk=1)
+		for count in range(1, 11):
+			post = Post(title="Post %d Title" % count,
+						text = "foo",
+						author=author)
+			if count < 6:
+				# publish the first five posts
+				pubdate = self.now - self.timedelta * count
+				post.published_date = pubdate
+			post.save()
--- mysite/blogging/urls.py
+++ mysite/blogging/urls.py
@@ -5,4 +5,5 @@ from blogging.views import stub_view
 
 urlpatterns = [
 	path('', stub_view, name="blog_index"),
+	path('posts/<int:post_id>/', stub_view, name="blog_detail")
 ]
