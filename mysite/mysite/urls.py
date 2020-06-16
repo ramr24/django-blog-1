1446d50d7aa23177dd63d2a4e538f9c7f6ce92ef
Ramkumar Rajanbabu <kumar.rajanbabu@gmail.com>
2020-06-15 17:38:43 -0700

    finished activity 7

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
 	path('', include('blogging.urls')),
 	path('polling/', include('polling.urls')),
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(template_name='login.html'), name="login"),
+    path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
 ]
