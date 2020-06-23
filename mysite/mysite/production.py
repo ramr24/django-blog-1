4793236c2a40e280dbe27ce4f19536249d884102
Ramkumar Rajanbabu <kumar.rajanbabu@gmail.com>
2020-06-23 13:06:32 -0700

    Added production file

---
+++ mysite/mysite/production.py
@@ -0,0 +1,18 @@
+# mysite/production.py
+
+import os
+
+import dj_database_url
+
+from .settings import *
+
+
+DATABASES = {
+    'default': dj_database_url.config(default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'))
+    }
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), 'localhost']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+SECRET_KEY = os.environ.get("dWeUrwRncvE5rNE9k4ukuyvthFGCWCNMHq5fMcMfaBKWQG9ePh")
